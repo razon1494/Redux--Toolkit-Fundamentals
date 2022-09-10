8d13dfd83110693d14b627b7dd084c36fbe78c93
razon1494 <razon1494@gmail.com>
2022-09-10 21:03:46 +0600

    test

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { fetchPosts } = require("./features/post/postSlice");
 store.subscribe(() => {});
 
 store.dispatch(fetchPosts());
-console.log("first");
+
 console.log("first");
 console.log("first");
 console.log("first");
